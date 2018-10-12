2c8ed955a37c055e21925ff479331a46335b7eb4
Pontus Virdebrant <povi01001@utb.vaxjo.se>
2018-10-12 13:39:15 +0200

    Start på listor

---
+++ Listor/Listor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listor
+{
+    class Program
+    {
+       
+            public class fukt<T>
+            {
+            protected T[] listning;
+            protected int swagger;
+            protected int snabb;
+            }
+
+            List<int> Listning = new List<int>();
+    }
+}
